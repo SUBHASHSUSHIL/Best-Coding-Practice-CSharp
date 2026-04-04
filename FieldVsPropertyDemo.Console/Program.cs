0794013365ab5c70903fc5de12bab147500f27fa
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-05 03:28:57 +0530

    Data ko direct expose mat karo, control ke saath expose karo

---
+++ FieldVsPropertyDemo.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace FieldVsPropertyDemo.Console
+{
+    // Never have public fields, use properties instead
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+
+    // BAD design
+    //class Person
+    //{
+    //    // Encapsulation break
+    //    // No validation, no control over the data
+    //    public string FirstName;
+    //    public string LastName;
+    //}
+
+    // Avoid this (extra method)
+    //public void SetFullName()
+    //{
+    //   FullName = FirstName + " " + LastName;
+    //}
+
+    // Better (Controlled access)
+    class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
