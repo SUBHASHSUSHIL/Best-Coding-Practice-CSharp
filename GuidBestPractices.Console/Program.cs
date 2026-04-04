d53ed2ed6891f5a5c07f04f88ab1a6d209ca5805
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-05 04:14:57 +0530

    Clean code = future-proof code

---
+++ GuidBestPractices.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GuidBestPractices.Console
+{
+    // Avoid using new Guid()
+    internal class Program
+    {
+        // Bad Examples
+        // This is not unique 00000000-0000-0000-0000-000000000000, but rather a GUID with all zeros, which is not the same as an empty GUID.
+        Guid id1 = new Guid();        // empty GUID
+        Guid id2 = new Guid("text");  // risky / invalid
+
+        // Correct Way (Use Guid.NewGuid()
+        Guid id = Guid.NewGuid();
+
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+
+            var orderId = Guid.NewGuid();
+            System.Console.WriteLine(orderId);
+        }
+    }
+}
