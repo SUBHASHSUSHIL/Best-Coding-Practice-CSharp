e48dfc4f116a0b965ad9fabd613139bc64e30b09
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-05 04:09:39 +0530

    Hardcoded string future me bug ban sakta hai

---
+++ NameofDemo.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace NameofDemo.Console
+{
+    // Use nameof() instead of hardcoding field names
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+
+            // Bad Example (Hardcoded string)
+            //System.Console.WriteLine(string.Format("Value: {0}", output));
+            // or
+            // throw new Exception("Invalid value for output");
+
+            // Best Practice (nameof())
+            //System.Console.WriteLine($"Value: {nameof(output)} = {output}");
+
+            // Example (Exception case)
+            // if (output < 0)
+            //{
+            //    throw new ArgumentException(nameof(output));
+            //}
+
+
+        }
+    }
+}
