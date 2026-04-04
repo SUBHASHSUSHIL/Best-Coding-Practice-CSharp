0d659663af9ada8c2ff002f9ae7a024da23456f6
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-04 21:24:33 +0530

    Readable code likhna fast code likhne se zyada important hai

---
+++ VariableDeclarationDemo.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace VariableDeclarationDemo.Console
+{
+    internal class Program
+    {
+        //public int DoOperation()
+        //{
+        //    int n1, n2, n3, n4;
+        //    int breakerValue;
+
+        //    n1 = 5;
+        //    n2 = 12;
+        //    breakerValue = 22;
+        //    n3 = n1 + n2;
+        //    n4 = n3 * breakerValue;
+
+        //    return n4;
+        //}
+
+        public int DoOperation()
+        {
+            int number1 = 5;  // Meaningful names and all variables not inialize single lines
+            int number2 = 12;
+            int breakerValue = 22;
+
+            int sum = number1 + number2;
+            int result = sum * breakerValue;
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
