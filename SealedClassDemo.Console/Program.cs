359e71b062c0f95ddd3a5a8dd2396347b6eeb915
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-04 21:50:24 +0530

    Jahan inheritance allowed hi nahi, wahan protected ka koi matlab nahi

---
+++ SealedClassDemo.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SealedClassDemo.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+
+    // Do not add protected members in sealed class
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    sealed class Employee : Person
+    {
+        //protected string EmployeeCode { get; set; }
+        private string EmployeeCode { get; set; }
+    }
+
+    class Accountant : Employee
+    {
+    }
+}
