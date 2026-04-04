83c72ac5a8c57a1b9295e5b30b7669ca134667ca
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-05 03:41:45 +0530

    Simple cheez ko simple hi rakho

---
+++ AutoPropertyDemo.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace AutoPropertyDemo.Console
+{
+    // Properties with no logic should be auto-implemented
+    internal class Program
+    {
+        // Bad Example
+        // Unnecessary code
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        // Best Practice (Auto property)
+        //public string Name { get; set; }
+
+
+        // When to use full properties:
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception();
+                _name = value;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
