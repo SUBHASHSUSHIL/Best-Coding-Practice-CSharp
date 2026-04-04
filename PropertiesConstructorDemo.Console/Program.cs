90826e6acb6ec9430dc9a8a7ccb5614693e02c0a
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-04 21:12:20 +0530

    Jo value kabhi change nahi hogi, usse change hone ka option hi mat do

---
+++ PropertiesConstructorDemo.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PropertiesConstructorDemo.Console
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; }  // This property only get
+
+        public Person(string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Age = DateTime.Now.Year - birthDate.Year;
+        }
+    }
+}
