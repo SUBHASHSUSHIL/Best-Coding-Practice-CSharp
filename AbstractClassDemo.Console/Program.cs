a64bbc47b70da14ce8610fdfa53620e7e65295f5
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-04 22:28:30 +0530

    Abstract class ka constructor sirf child ke liye hota hai, duniya ke liye nahi

---
+++ AbstractClassDemo.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AbstractClassDemo.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+
+    abstract class Person
+    {
+        protected Person() { }
+    }
+
+    class Employee : Person
+    {
+    }
+}
