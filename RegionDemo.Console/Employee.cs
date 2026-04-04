c05aee9d1e8880d71a5e813f9a1251acf9e6749a
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-04 17:01:31 +0530

    Reason organizes the code, but clean design is even more important

---
+++ RegionDemo.Console/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionDemo.Console
+{
+    public class Employee
+    {
+        #region Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        #endregion
+
+        #region Constructor
+        public Employee(string firstName, string lastName, string role)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Role = role;
+        }
+        #endregion
+
+        #region Public Methods
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+        #endregion
+
+        #region Private Methods
+        private void Log()
+        {
+            // some logic
+        }
+        #endregion
+    }
+}
---
+++ RegionDemo.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RegionDemo.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
