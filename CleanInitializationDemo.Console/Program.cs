4905900393ebc3fe17eab5aed2d13455cb2db886
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-04 21:37:27 +0530

    Remove member initialization if they are being set before being used

---
+++ CleanInitializationDemo.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CleanInitializationDemo.Console
+{
+    internal class Program
+    {
+        // Remove member initialization if they are being set before being used
+        public int GetValue()
+        {
+            int n1 = int.MinValue;
+            int n2 = int.MinValue;
+
+            n1 = GetNumber1();
+            n2 = GetNumber1();
+
+            return n1 + n2;
+        }
+
+        private int GetNumber1()
+        {
+            return 5;
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
