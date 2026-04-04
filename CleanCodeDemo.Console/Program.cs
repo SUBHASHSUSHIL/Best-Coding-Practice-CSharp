7fdd71caafdbbeb9e524eac083500bc16acce9c8
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-04 22:17:11 +0530

    Code me history mat rakho, history Git me rakho

---
+++ CleanCodeDemo.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CleanCodeDemo.Console
+{
+    internal class Program
+    {
+        // Never leave commented-out code if you are using a source control system
+        public int Calculate()
+        {
+            int result = 0;
+            int a = 0;
+            int b = 0;
+
+            // Old code ko comment karke mat rakho (Bad Example (Common Mistake))
+            // Old logic (not used anymore)
+            // result = a + b;
+
+            // New logic
+            result = a * b;
+
+            return result;
+        }
+
+        // Correct Way
+        //public int Calculate()
+        //{
+        //    int a = 0;
+        //    int b = 0;
+
+        //    int result = a * b;
+        //    return result;
+        //}
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
