d1e98ad01d3996c663a81c2f5e1b1987dfaa4082
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-04 16:56:27 +0530

    The enum is in the code, but its impact extends to the database

---
+++ EnumDemo.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EnumDemo.Console
+{
+    internal class Program
+    {
+        public enum DaysOfWeek
+        {
+            Sunday = 1,
+            Monday = 2,
+            Tuesday = 3,
+            Wednesday = 4,
+            Thursday = 5,
+            Friday = 6,
+            Saturday = 7
+        }
+
+        public void GetWeekday(Int32 weekdays)
+        {
+            DaysOfWeek daysOfWeek = (DaysOfWeek)weekdays;
+            PrintDay(daysOfWeek);
+        }
+
+        public void PrintDay(DaysOfWeek day)
+        {
+            System.Console.WriteLine($"The day of the week is: {day}");
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.GetWeekday(3);
+        }
+    }
+}
