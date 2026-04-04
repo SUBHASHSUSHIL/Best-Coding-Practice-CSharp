d5aa04622608057912381a39a356744ec05a4e54
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-04 17:45:36 +0530

    Uninitialized variable bug ka ghar hota hai, default value safety ka shield hai

---
+++ DefaultValuesDemo.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DefaultValuesDemo.Console
+{
+    internal class Program
+    {
+        public void ProcessRule(int value2)
+        {
+            int value1 = 0; // default value
+
+            if (value2 > 5)
+            {
+                value1 = 6;
+            }
+            else
+            {
+                value1 = 10;
+            }
+
+            System.Console.Write(value1);
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+
+            Program program = new Program();
+            program.ProcessRule(3); // Output: 10
+
+            program.ProcessRule(6); // Output: 6
+        }
+    }
+}
