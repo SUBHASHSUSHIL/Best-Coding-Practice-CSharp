cf28540fab81f34efd6a250bee95d3d23bfdcad6
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-05 00:27:03 +0530

    Jitna kam nesting, utna clean code

---
+++ IfStatementOptimizationDemo.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IfStatementOptimizationDemo.Console
+{
+    internal class Program
+    {
+
+        //Merge nested if statements to improve readability
+
+        //public void DoOperation(int n1, int n2)
+        //{
+        //    if (n1 > 5)
+        //    {
+        //        if(n2 < 10)
+        //        {
+        //            // Do Something
+        //        }
+        //    }
+        //}
+
+        public void DoOperation(int n1, int n2)
+        {
+            if (n1 > 5 && n2 < 10)
+            {
+                // Do Something
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
