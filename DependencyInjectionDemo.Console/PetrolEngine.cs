d39dc522a93799efe5d8933f6d5607fb4130a2c2
SUSHIL THAKUR <mt3028788@gmail.com>
2026-04-06 01:48:15 +0530

    Object khud dependency create na kare, bahar se le — ye hi DI hai

---
+++ DependencyInjectionDemo.Console/PetrolEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo.Console
+{
+    public class PetrolEngine : IEngine
+    {
+        public void Run()
+        {
+            System.Console.WriteLine("Petrol engine running");
+        }
+    }
+}
---
+++ DependencyInjectionDemo.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DependencyInjectionDemo.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+
+            IEngine engine = new PetrolEngine();
+            Car car = new Car(engine);
+
+            car.Start();
+        }
+
+        // DI:- 
+        // Loosely coupled, easier to test, more maintainable, better separation of concerns, and improved flexibility.
+    }
+}
